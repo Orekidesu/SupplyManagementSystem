2fffbf889c2eec3590fd4b6110e3695f3f168d1a
Orekidesu <cmcodog10@gmail.com>
2024-08-09 11:30:49 +0800

    Transaction Page
     - Transaction Tabs

--- resources/views/pages/transactions/index.blade.php
+++ resources/views/pages/transactions/index.blade.php
@@ -11,7 +11,43 @@
         </div>
     </div>
 
-    <h1 class="text-primary">
-        <center>This is the transactions page</center>
-    </h1>
+
+    <div x-data="{
+        openTab: 1,
+        activeClasses: 'border-l border-t border-r rounded-t text-black underline ',
+        inactiveClasses: 'text-primary hover:text-blue-800 '
+    }" class="mx-auto sm:px-6 lg:px-8 ">
+        <ul class="flex justify-center">
+            <li @click="openTab = 1" :class="{ '-mb-px': openTab === 1 }" class="-mb-px mr-1">
+                <a :class="openTab === 1 ? activeClasses : inactiveClasses"
+                    class="bg-white inline-block py-2 px-4 font-semibold" href="#">
+                    Property Acknowledgement Report
+                </a>
+            </li>
+            <li @click="openTab = 2" :class="{ '-mb-px': openTab === 2 }" class="mr-1">
+                <a :class="openTab === 2 ? activeClasses : inactiveClasses"
+                    class="bg-white inline-block py-2 px-4 font-semibold" href="#">Requsition Slip</a>
+            </li>
+            <li @click="openTab = 3" :class="{ '-mb-px': openTab === 3 }" class="mr-1">
+                <a :class="openTab === 3 ? activeClasses : inactiveClasses"
+                    class="bg-white inline-block py-2 px-4 font-semibold" href="#">Purchace order</a>
+            </li>
+            <li @click="openTab = 4" :class="{ '-mb-px': openTab === 4 }" class="mr-1">
+                <a :class="openTab === 4 ? activeClasses : inactiveClasses"
+                    class="bg-white inline-block py-2 px-4 font-semibold" href="#">Acknowledgement Report</a>
+            </li>
+            <li @click="openTab = 5" :class="{ '-mb-px': openTab === 5 }" class="mr-1">
+                <a :class="openTab === 5 ? activeClasses : inactiveClasses"
+                    class="bg-white inline-block py-2 px-4 font-semibold" href="#">Condemning of Equipment</a>
+            </li>
+        </ul>
+        <div class="w-full pt-4 text-black flex justify-center">
+            <div x-show="openTab === 1">Tab #1</div>
+            <div x-show="openTab === 2">Tab #2</div>
+            <div x-show="openTab === 3">Tab #3</div>
+            <div x-show="openTab === 4">Tab #4</div>
+            <div x-show="openTab === 5">Tab #5</div>
+        </div>
+    </div>
+
 </x-app-layout>
